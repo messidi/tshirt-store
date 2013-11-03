79e0a9031a3d8b88fae0e8a7286dc957bbd05c42
messidi <mohamed.elmessidi@gmail.com>
2013-11-03 18:53:39 +0300

    web.js edit

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ var buffer = fs.readFileSync("index.html");
 response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
